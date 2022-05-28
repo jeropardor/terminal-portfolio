7acf681384aa4d60689ab4ef874b7e406905fa99
Jero <jeropardor3@gmail.com>
2022-05-28 13:40:27 +0200

    cmd Echo

--- js/main.js
+++ js/main.js
@@ -21,6 +21,41 @@ host.innerHTML = hostName;
 appWindow.focus();
 
 
+
+function print(text, hl = false) {
+    let res = document.createElement('p');
+
+    res.innerHTML = text;
+
+    if (hl) res.classList.add('hl1');
+    results.appendChild(res);
+}
+
+
+// cmds ------------------------------------------------------------------------
+
+function cmdEcho(argv) {
+    print(argv.join(' '), true);
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+function executeCmd(text) {
+    const argv = text.split(' ');
+    const cmd = argv.shift();
+    switch (cmd) {
+        case 'echo':
+            cmdEcho(argv);
+            break;
+
+        default:
+            break;
+    }
+}
+
+
 function processCmd(text) {
     let res = document.createElement('p');
 
@@ -37,29 +72,35 @@ function processCmd(text) {
     results.appendChild(res);
 
     appWindow.scrollTop = appWindow.scrollHeight;
+
+    executeCmd(text);
 };
 
 
-appWindow.addEventListener('keydown', c => {
+appWindow.addEventListener('keypress', c => {
     switch (c.key) {
         case 'Enter': // trigers the processCmd function
             processCmd(input.innerHTML);
             input.innerHTML = '';
             break;
 
-        case 'Tab':
-        case 'Shift':
-        case 'Control': // resets the run
-            c.preventDefault();
-            break;
-
-        case 'Backspace': // resets the run
-            input.innerHTML = input.innerHTML.slice(0, -1);
-            break;
-
-
         default: // registers a normal char
             input.innerHTML += c.key;
-            // console.log(c.key);
     }
+});
+
+
+appWindow.addEventListener('keydown', e => {
+    if (e.key === 'Delete' || e.key === 'Backspace') {
+        input.innerHTML = input.innerHTML.slice(0, -1);
+    }
+});
+
+
+appWindow.addEventListener('paste', e => {
+    let paste = (e.clipboardData || window.clipboardData).getData('text');
+
+    input.innerHTML += paste;
+
+    e.preventDefault();
 });
